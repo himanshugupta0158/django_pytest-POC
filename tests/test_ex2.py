095fb7aed60639d619e560a96ac3296819e55b75
himanshugupta0158 <himanshugupta0158@gmail.com>
2022-02-03 18:03:45 +0530

    paramtrized fixtures test

--- tests/test_ex2.py
+++ tests/test_ex2.py
@@ -5,9 +5,11 @@ def test_new_user(create_newuser):
     print(create_newuser.username)
     assert True
     
-'''
+
 def test_product(db, product_factory):
     product = product_factory.create()
     print(product.description)
     assert True
+    
+'''
     
